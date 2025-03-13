1b0c75668480b512bca01f8dc10650b34ffefe57
Tommax2 <martinsolumi@gmail.com>
2025-03-13 14:05:16 +0100

    add

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ cloudinary.config({
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://jims-touch.onrender.com'
+    origin: ['https://jims-touch.onrender.com', 'http://localhost:5174']
 }));
 
 // Ensure upload directory exists
